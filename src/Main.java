8ac7b67c9651c1d654b6054f25492ab268d9f81e
KhonDuong <duongdilan@yahoo.com>
2023-03-20 21:18:31 -0400

    Added more comments.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -10,15 +12,8 @@ public class Main {
     // Code is intended to output the top 20 most used words in between the two lines indicated.
     public static void main(String[] args)
     {
-        // You can change the poem HTML file.
-        String file = "The Project Gutenberg eBook of The Raven, by Edgar Allan Poe.htm";
-        String output;
-
         Main main = new Main();
-        output = main.getHTMLAsString(file);
-        // You can change the start and end to work with another poem.
-        output = main.filterToPoem(output, "The Raven", "Shall be lifted nevermore");
-        main.mostUsedWords(output);
+        main.createUI();
     }
 
     /* HTML can't be used with scanner directly,
@@ -97,7 +92,7 @@ public class Main {
     * Sorts values of hashmap using an arraylist
     * Prints out top 20 most used words
     */
-    public void mostUsedWords(String filteredPoem)
+    public ArrayList<Map.Entry<String, Integer>> mostUsedWords(String filteredPoem)
     {
         HashMap<String, Integer> wordOccurrenceMap = new HashMap<>();
 
@@ -116,8 +111,116 @@ public class Main {
         ArrayList<Map.Entry<String, Integer>> wordOccurrenceList = new ArrayList<>(wordOccurrenceMap.entrySet());
         Collections.sort(wordOccurrenceList, Map.Entry.comparingByValue(Comparator.reverseOrder())); // Reverse order for DESC. Normally ASC.
 
-        for (int i = 0; i < 20; i++) {
-            System.out.println(wordOccurrenceList.get(i)); // Output top 20 most used words.
-        }
+        return wordOccurrenceList;
+    }
+
+    public void createUI()
+    {
+        // Create frame
+        JFrame frame = new JFrame("Find out the top 20 used words in your poem!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(600, 420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+
+        // Create instructions for how to use the program
+        JLabel instructionLabel = new JLabel();
+        instructionLabel.setBounds(100, 25, 400, 25);
+        instructionLabel.setVerticalAlignment(JLabel.CENTER);
+        instructionLabel.setHorizontalAlignment(JLabel.CENTER);
+        instructionLabel.setText("Choose the HTM or HTML file of your poem, then write out");
+        frame.add(instructionLabel);
+
+        JLabel instructionLabel2 = new JLabel();
+        instructionLabel2.setBounds(100, 50, 400, 25);
+        instructionLabel2.setVerticalAlignment(JLabel.CENTER);
+        instructionLabel2.setHorizontalAlignment(JLabel.CENTER);
+        instructionLabel2.setText("just the letters of the title and ending lines in the correct fields.");
+        frame.add(instructionLabel2);
+
+        // Choose file, but only when the openFileButton is pressed
+        JFileChooser chooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("HTML File", "htm", "html");
+        chooser.setFileFilter(filter);
+
+        // Don't let the user try to parse a poem without even selecting a file first
+        JButton parsePoemButton = new JButton();
+        parsePoemButton.setBounds(232, 250, 132, 25);
+        parsePoemButton.setText("Parse Poem");
+        parsePoemButton.setEnabled(false);
+
+        // If file is chosen, disable the button and let them know they picked a proper HTML file.
+        JButton openFileButton = new JButton();
+        openFileButton.setBounds(200, 75, 200, 45);
+        openFileButton.setText("Choose HTM or HTML File");
+        openFileButton.addActionListener(e -> {
+            int returnVal = chooser.showOpenDialog(null);
+            if (returnVal == JFileChooser.APPROVE_OPTION) {
+                openFileButton.setText("HTML file picked!");
+                openFileButton.setEnabled(false);
+                parsePoemButton.setEnabled(true);
+            }
+        });
+        frame.add(openFileButton);
+
+        // Labels and fields for the start and end of the poem
+        JLabel poemTitleFieldLabel = new JLabel();
+        poemTitleFieldLabel.setBounds(126, 130, 100, 25);
+        poemTitleFieldLabel.setText("Poem Title: ");
+        frame.add(poemTitleFieldLabel);
+
+        JTextField poemTitleField = new JTextField();
+        poemTitleField.setBounds(200, 130, 200, 30);
+        frame.add(poemTitleField);
+
+        JLabel poemEndFieldLabel = new JLabel();
+        poemEndFieldLabel.setBounds(126, 170, 100, 25);
+        poemEndFieldLabel.setText("Poem End: ");
+        frame.add(poemEndFieldLabel);
+
+        JTextField poemEndField = new JTextField();
+        poemEndField.setBounds(200, 170, 200, 30);
+        frame.add(poemEndField);
+
+        // Final text label uses HTML to make use of <br> to create newlines.
+        JLabel displayTopTwentyLabel = new JLabel();
+        displayTopTwentyLabel.setBounds(25, 0, 400, 380);
+
+        parsePoemButton.addActionListener(e -> {
+            if (poemTitleField.getText() != null && !poemTitleField.getText().trim().isEmpty() && poemEndField.getText() != null && !poemEndField.getText().trim().isEmpty()) {
+                // This order of functions used to be in main to get the file, filter it down to just the contents of the poem, and then to output the most used words in the poem.
+                String output = this.getHTMLAsString(chooser.getSelectedFile().getName());
+                output = this.filterToPoem(output, poemTitleField.getText(), poemEndField.getText());
+                ArrayList<Map.Entry<String, Integer>> wordOccurrenceList = this.mostUsedWords(output);
+
+                // Create the text that goes into the final label of top twenty words.
+                String htmlText = "<html>Here are the top twenty most used words in your poem.<br><br>";
+                for (int i = 0; i < 20; i++) {
+                    htmlText = htmlText.concat(Integer.toString(i + 1) + ". " + wordOccurrenceList.get(i) + "<br>");
+                    // System.out.println(wordOccurrenceList.get(i)); // Output top 20 most used words.
+                }
+                htmlText = htmlText.concat("</html>");
+                displayTopTwentyLabel.setText(htmlText);
+
+                // Hide all the old instructions and display the output.
+                instructionLabel.setVisible(false);
+                instructionLabel2.setVisible(false);
+                parsePoemButton.setVisible(false);
+                openFileButton.setVisible(false);
+                poemTitleFieldLabel.setVisible(false);
+                poemTitleField.setVisible(false);
+                poemEndFieldLabel.setVisible(false);
+                poemEndField.setVisible(false);
+
+                frame.add(displayTopTwentyLabel);
+                frame.repaint();
+                frame.revalidate();
+            }
+        });
+        frame.add(parsePoemButton);
+
+        frame.repaint();
+        frame.revalidate();
     }
 }
